3a8bac45d2cebd0ed1e8ffd7f289c184d565e9e7
ArunFSD <arun.techcode.com>
2026-03-03 13:05:16 +0530

    Moved the package

--- src/main/java/com/finsphere/config/ConfigServerApplication.java
+++ src/main/java/com/finsphere/config/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.finsphere.config_server;
+package com.finsphere.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/finsphere/config/ConfigServerApplicationTests.java
+++ src/test/java/com/finsphere/config/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.finsphere.config_server;
+package com.finsphere.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
