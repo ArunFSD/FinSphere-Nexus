7a4aa251130e59c9bb3227ea70254fb210ba9f4b
arun-appiyo <arun.techcode@gmail.com>
2026-04-29 18:37:43 +0530

    Changes and added exclusion in SpringBootApplication

--- src/main/java/com/finsphere/GatewayServiceApplication.java
+++ src/main/java/com/finsphere/GatewayServiceApplication.java
@@ -1,15 +1,20 @@
 package com.finsphere;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        ReactiveSecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class,      // Add this
+        ReactiveManagementWebSecurityAutoConfiguration.class // Add this
+})
 public class GatewayServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 }
